bc9efefbeda4880727aa308a30b4be149a99390e
maker233 <javiermaker@gmail.com>
2018-01-26 02:36:29 +0100

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public title = 'app';
+  public color = "'red'"
+
+
 }
